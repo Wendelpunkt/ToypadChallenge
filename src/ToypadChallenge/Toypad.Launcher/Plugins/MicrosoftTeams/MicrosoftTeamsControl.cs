0644134978cd36f91f38836479346b64d8a2c92e
Tom Wendel <tom.wendel@antme.net>
2024-08-14 14:16:29 +0200

    First empty draft of the microsoft teams plugin

---
+++ src/ToypadChallenge/Toypad.Launcher/Plugins/MicrosoftTeams/MicrosoftTeamsControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Toypad.Launcher.Plugins.MicrosoftTeams
+{
+    public partial class MicrosoftTeamsControl : UserControl
+    {
+        public MicrosoftTeamsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
