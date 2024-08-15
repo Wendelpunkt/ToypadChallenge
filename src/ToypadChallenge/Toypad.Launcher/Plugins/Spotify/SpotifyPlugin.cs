dd74077c156f111303c365242426a59e41b96263
Yannick Schrade <yannick1218@outlook.com>
2024-08-15 16:36:39 +0200

    Added empty base classes for the spotify plugin

---
+++ src/ToypadChallenge/Toypad.Launcher/Plugins/Spotify/SpotifyPlugin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toypad.Launcher.Plugins.Spotify;
+
+[PluginDescription("Spotify", "Integrates Spotify to play your Favorit songs on your devices")]
+public class SpotifyPlugin : Plugin<SpotifyConfiguration>
+{
+    public override Control Control { get; }
+
+    public override void Dispose()
+    {
+
+    }
+
+    protected override SpotifyConfiguration GetDefaultConfiguration()
+    {
+        return new SpotifyConfiguration();
+    }
+
+    protected override void SetConfiguration(SpotifyConfiguration configuration)
+    {
+
+    }
+
+    protected override void SetToypad(IToypad toypad)
+    {
+
+    }
+
+    protected override void UpdateConfiguration()
+    {
+
+    }
+}
